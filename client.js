5e55ffd96191617a835d0b7d3b68df7f0ba370f6
ameba23 <ameba23@systemli.org>
2020-12-04 21:17:58 +0100

    fix client

--- client.js
+++ client.js
@@ -3,19 +3,29 @@ const net = require('net')
 
 const swarm = hyperswarm()
 
-module.exports = function (topic, port) {
-  let openSwarmConnection
-  let openHttpConnection
+module.exports = function (topic, port = 3001) {
+  let swarmConnection
 
   const server = net.createServer((connection) => {
-    openHttpConnection = connection
-    connection.on('data', (data) => {
-      console.log(data.toString())
-      if (openSwarmConnection) openSwarmConnection.write(data)
-    })
-    connection.on('end', () => {
-      openHttpConnection = false
-    })
+    if (swarmConnection) {
+      connection.on('data', (data) => {
+        console.log(data.toString())
+        swarmConnection.write(data)
+      })
+      connection.on('end', () => {
+        console.log('ending')
+        // remove this listener?
+      })
+      swarmConnection.on('data', (data) => {
+        if (!connection.destroyed) connection.write(data)
+      })
+      swarmConnection.on('close', () => {
+        connection.end()
+      })
+      connection.on('error', (err) => {
+        console.error(err)
+      })
+    }
   })
 
   server.on('error', (err) => {
@@ -28,10 +38,18 @@ module.exports = function (topic, port) {
 
   swarm.on('connection', (connection) => {
     console.log('swarm connection')
-    openSwarmConnection = connection
-    connection.on('data', (data) => {
-      if (openHttpConnection) openHttpConnection.write(data)
-    })
+    swarmConnection = connection
   })
   swarm.join(topic, { announce: false, lookup: true })
+  logEvents(swarm)
+}
+
+function logEvents (emitter, name) {
+  const emit = emitter.emit
+  name = name ? `(${name}) ` : ''
+  emitter.emit = (...args) => {
+    // console.log(`\x1b[33m${args[0]}\x1b[0m`, util.inspect(args.slice(1), { depth: 1, colors: true }))
+    console.log(`\x1b[33m${args[0]}\x1b[0m`)
+    emit.apply(emitter, args)
+  }
 }
