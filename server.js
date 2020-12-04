f4be88716ad1bc36d27f5d0c052350a7617fd93a
ameba23 <ameba23@systemli.org>
2020-12-04 21:18:08 +0100

    fix server

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const hyperswarm = require('hyperswarm')
 const net = require('net')
-
+// const { pipeline } = require('stream')
 const swarm = hyperswarm()
 
 module.exports = function (topic, port) {
   swarm.on('connection', (connection) => {
     console.log('swarm connection')
-    connection.on('data', (data) => {
-      const client = net.createConnection({ port }, () => {
+    const client = net.createConnection({ port }, () => {
+      connection.on('data', (data) => {
         client.write(data)
       })
       client.on('data', (data) => {
         connection.write(data)
+      })
+      connection.on('end', () => {
         client.end()
       })
     })
+    client.on('error', (err) => {
+      console.error(err)
+    })
   })
   swarm.join(topic, { announce: true, lookup: false })
+  console.log('Forwarding requests to port', port)
+}
+function logEvents (emitter, name) {
+  const emit = emitter.emit
+  name = name ? `(${name}) ` : ''
+  emitter.emit = (...args) => {
+    // console.log(`\x1b[33m${args[0]}\x1b[0m`, util.inspect(args.slice(1), { depth: 1, colors: true }))
+    console.log(`\x1b[33m${args[0]}\x1b[0m`)
+    emit.apply(emitter, args)
+  }
 }
